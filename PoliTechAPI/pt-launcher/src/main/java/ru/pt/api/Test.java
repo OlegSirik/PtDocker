06c99dd37459b332fd6b7ad59400493aa9f7a0b8
RomanStudenskiy <studensky.r@yandex.ru>
2025-12-01 10:28:21 +0300

    Fix pf

--- PoliTechAPI/pt-launcher/src/main/java/ru/pt/api/Test.java
+++ PoliTechAPI/pt-launcher/src/main/java/ru/pt/api/Test.java
@@ -1,30 +1,15 @@
 package ru.pt.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.micrometer.common.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.pt.api.dto.auth.Account;
-import ru.pt.api.dto.exception.InternalServerErrorException;
-import ru.pt.api.dto.exception.NotFoundException;
-import ru.pt.api.dto.product.LobModel;
-import ru.pt.api.dto.product.LobVar;
 import ru.pt.api.security.SecuredController;
 import ru.pt.api.service.auth.AccountService;
-import ru.pt.api.service.file.FileService;
-import ru.pt.api.service.process.PreProcessService;
 import ru.pt.api.service.process.ProcessOrchestrator;
 import ru.pt.auth.security.SecurityContextHelper;
-import ru.pt.db.repository.PolicyIndexRepository;
-import ru.pt.db.repository.PolicyRepository;
-import ru.pt.product.repository.ProductRepository;
-import ru.pt.product.repository.ProductVersionRepository;
 
 import java.util.*;
 
@@ -39,32 +24,16 @@ import java.util.*;
 @RequestMapping("/api/v1/{tenant-code}/test")
 public class Test extends SecuredController {
 
-    private static final Logger logger = LoggerFactory.getLogger(Test.class);
-
     private final AccountService accountService;
     private final ProcessOrchestrator processOrchestrator;
-    private final PolicyIndexRepository policyIndexRepository;
-    private final PolicyRepository policyRepository;
-    private final ProductVersionRepository productVersionRepository;
-    private final ProductRepository productRepository;
-    private final FileService fileService;
-    private final PreProcessService preProcessService;
 
     protected Test(
         SecurityContextHelper securityContextHelper, AccountService accountService,
-        ProcessOrchestrator processOrchestrator, PolicyIndexRepository policyIndexRepository, PolicyRepository policyRepository,
-        ProductVersionRepository productVersionRepository, ProductRepository productRepository,
-        FileService fileService, PreProcessService preProcessService
+        ProcessOrchestrator processOrchestrator
     ) {
         super(securityContextHelper);
         this.accountService = accountService;
         this.processOrchestrator = processOrchestrator;
-        this.policyIndexRepository = policyIndexRepository;
-        this.policyRepository = policyRepository;
-        this.productVersionRepository = productVersionRepository;
-        this.productRepository = productRepository;
-        this.fileService = fileService;
-        this.preProcessService = preProcessService;
     }
 
     @PostMapping("/create-client")
@@ -133,78 +102,5 @@ public class Test extends SecuredController {
             @RequestBody String requestBody) {
         String result = processOrchestrator.save(requestBody);
         return ResponseEntity.ok(result);
-    }
-
-    @PostMapping("/policy/printpf/{policy-nr}/{pf-type}")
-    public byte[] printPolicy(
-        @PathVariable("policy-nr") String policyNr,
-        @PathVariable("pf-type") String pfType,
-        @PathVariable("tenant-code") String tenantCode) throws JsonProcessingException {
-        
-        var policyIndex = policyIndexRepository
-            .findByPolicyNumber(policyNr)
-            .orElseThrow(() ->
-                new NotFoundException("Не удалось найти полис по номеру - %s".formatted(policyNr))
-            );
-        var policy = policyRepository.findById(policyIndex.getPolicyId())
-            .orElseThrow(() ->
-                new NotFoundException("Не удалось найти полис по id - %s".formatted(policyIndex.getPolicyId()))
-            );
-        var productId = productRepository.findByCodeAndIsDeletedFalse(policyIndex.getProductCode())
-            .orElseThrow(() ->
-                new NotFoundException(
-                    "Не удалось найти продукт; productCode %s".formatted(policyIndex.getProductCode())
-                )
-            ).getId();
-
-        var productVersion = productVersionRepository.findByProductIdAndVersionNo(
-            productId, policyIndex.getVersionNo()
-        ).orElseThrow(() ->
-            {
-                logger.error(
-                    "Не удалось найти версию продукта; productId {}, versionNo {}",
-                    productId, policyIndex.getVersionNo()
-                );
-//                return new InternalServerErrorException("Обратитесь к администратору");
-                return new InternalServerErrorException(
-                    "Не удалось найти версию продукта; productId %s, versionNo %s".formatted(
-                    productId, policyIndex.getVersionNo())
-                );
-            }
-        );
-
-        var json = productVersion.getProduct();
-        ArrayNode context = (ArrayNode) new ObjectMapper().readTree(json).get("vars");
-
-        var lobModel = new LobModel();
-        List<LobVar> vars = new LinkedList<>();
-
-        for (int i = 0; i < context.size(); i++) {
-            JsonNode val = context.get(i);
-            try {
-                vars.add(new ObjectMapper().readValue(val.asText(), LobVar.class));
-            } catch (JsonProcessingException e) {
-                logger.error("Не удалось спарcить productVersion.product.vars[{}]", i);
-//                throw new InternalServerErrorException("Обратитесь к администратору");
-                throw new InternalServerErrorException(
-                    "Не удалось спарcить productVersion.product.vars[%s]".formatted(i)
-                );
-            }
-        }
-
-        lobModel.setMpVars(vars);
-        vars = preProcessService.evaluateAndEnrichVariables(policy.getPolicy(), lobModel, productVersion.getProduct());
-        Map<String, String> keyValues = new HashMap<>();
-
-        for (LobVar node : vars) {
-            String key = node.getVarCode();
-            String value = node.getVarValue();
-            if (StringUtils.isBlank(value)) {
-                value = node.getVarPath();
-            }
-            System.out.println(key + " " + value);
-            keyValues.put(key, value);
-        }
-        return fileService.getFile(pfType, keyValues);
     }
 }
