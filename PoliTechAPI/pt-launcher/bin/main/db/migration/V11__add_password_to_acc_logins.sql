8aafebe1b57173882c32ef3d5cd8b95e7aabe11b
Oleg Sirik <oleg.sirik@mail.ru>
2025-12-03 09:24:58 +0300

    Exclude pt-launcher/bin directory from git tracking

--- PoliTechAPI/pt-launcher/bin/main/db/migration/V11__add_password_to_acc_logins.sql
+++
@@ -1,9 +0,0 @@
--- Add password_hash field to acc_logins table for simple authentication
--- Migration V11
-
-ALTER TABLE acc_logins
-ADD COLUMN password_hash VARCHAR(255);
-
--- Add comment to the column
-COMMENT ON COLUMN acc_logins.password_hash IS 'BCrypt hashed password for simple authentication';
-
