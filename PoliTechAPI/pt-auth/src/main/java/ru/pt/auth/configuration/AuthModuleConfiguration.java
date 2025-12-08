052132fd3fb48007cbe6606841c7d4e270084486
SavchenkoDV <svk.dmitriy@gmail.com>
2025-12-08 13:39:49 +0300

    fix associated accounts in response

--- PoliTechAPI/pt-auth/src/main/java/ru/pt/auth/configuration/AuthModuleConfiguration.java
+++ PoliTechAPI/pt-auth/src/main/java/ru/pt/auth/configuration/AuthModuleConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("ru.pt.auth")
+@ComponentScan({"ru.pt.auth", "ru.pt.api"})
 public class AuthModuleConfiguration {
 }
